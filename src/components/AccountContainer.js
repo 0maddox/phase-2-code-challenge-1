b6c9218c669cbda9416086e657b7a6fa3d1994f8
0maddox <nickkiim7@gmail.com>
2022-09-30 14:18:39 +0300

    final commit

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,16 +3,16 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
-// function AccountContainer() {
-//   const [transactions,setTransactions] = useState([])
-//   const [search,setSearch] = useState("")
-//   return (
-//     <div>
-//       <Search  search = {search} setSearch ={setSearch}/>
-//       <AddTransactionForm  transactions={transactions} setTransactions ={setTransactions}/>
-//       <TransactionsList transactions={transactions} setTransactions ={setTransactions}  search={search} setSearch={setSearch}/>
-//     </div>
-//   );
-// }
+function AccountContainer() {
+  const [transactions,setTransactions] = useState([])
+  const [search,setSearch] = useState("")
+  return (
+    <div>
+      <Search  search = {search} setSearch ={setSearch}/>
+      <AddTransactionForm  transactions={transactions} setTransactions ={setTransactions}/>
+      <TransactionsList transactions={transactions} setTransactions ={setTransactions}  search={search} setSearch={setSearch}/>
+    </div>
+  );
+}
 
 export default AccountContainer;
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,41 +1,41 @@
 import React,{useState} from "react";
 import Transaction from "./Transaction";
 
-// function TransactionsList({ transactions, setTransactions, search, setSearch }) {
-//   useEffect(() => {
-//     fetch("http://localhost:8001/transactions")
-//       .then((r) => r.json())
-//       .then((data) => setTransactions(data))
-//   },
-//   [setTransactions]
-//   )
-//   const filteredTransactions =transactions.filter((transactions) => transactions.description.toLowerCase().includes(search.toLowerCase()))
+function TransactionsList({ transactions, setTransactions, search, setSearch }) {
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions")
+      .then((r) => r.json())
+      .then((data) => setTransactions(data))
+  },
+  [setTransactions]
+  )
+  const filteredTransactions =transactions.filter((transactions) => transactions.description.toLowerCase().includes(search.toLowerCase()))
 
-//   const transactionList =filteredTransactions.map((transactions)=>{
-//     return
-//     <transactions transactions={transactions} setTransactions={setTransactions} key={transaction.id} date={transaction.date} description={transaction.description} category={transaction.category} amount={transaction.amount} id={transaction.id}/>
-//  })
-//   return (
-//     <table className="ui celled striped padded table">
-//       <tbody>
-//         <tr>
-//           <th>
-//             <h3 className="ui center aligned header">Date</h3>
-//           </th>
-//           <th>
-//             <h3 className="ui center aligned header">Description</h3>
-//           </th>
-//           <th>
-//             <h3 className="ui center aligned header">Category</h3>
-//           </th>
-//           <th>
-//             <h3 className="ui center aligned header">Amount</h3>
-//           </th>
-//         </tr>
-//         {/* render a list of <Transaction> components here */}
-//       </tbody>
-//     </table>
-//   );
-// }
+  const transactionList =filteredTransactions.map((transactions)=>{
+    return
+    <transactions transactions={transactions} setTransactions={setTransactions} key={transaction.id} date={transaction.date} description={transaction.description} category={transaction.category} amount={transaction.amount} id={transaction.id}/>
+ })
+  return (
+    <table className="ui celled striped padded table">
+      <tbody>
+        <tr>
+          <th>
+            <h3 className="ui center aligned header">Date</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Description</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Category</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Amount</h3>
+          </th>
+        </tr>
+        {/* render a list of <Transaction> components here */}
+      </tbody>
+    </table>
+  );
+}
 
 export default TransactionsList;
